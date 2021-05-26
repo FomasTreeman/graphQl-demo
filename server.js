45ffee2e69e190e4e35b04de506b6ecb8c640251
FomasTreeman <tom@team-freeman.com>
2021-05-27 00:32:20 +0100

    please work

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const express = require("express");
 
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("this is working");
+})
+
+app.listen(3000, ()=> {
+    console.log("app is runnign on port 3000");
+})
+
 
